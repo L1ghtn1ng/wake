9e56fa53477c1bc71942076a743fd71d8e304a14
L1ghtn1ng <townsend891@hotmail.com>
2019-08-04 02:17:12 +0100

    WIP UI update and better way to manage adding new computers

--- wol/wake.py
+++ wol/wake.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # Copyright Jay Townsend 2018-2019
 
+import yaml
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -9,17 +10,20 @@ from wakeonlan import *
 app = Flask(__name__)
 
 
+class Computers:
+    @staticmethod
+    def config():
+        with open('computers.yaml', 'r') as config:
+            return yaml.safe_load(config)['computers']
+
+
 @app.route('/')
 def homepage():
-    return render_template('index.html')
 
-
-@app.route('/mac', methods=['POST'])
-def send_mac():
-    mac = request.form['macaddr']
-    send_magic_packet(mac)
-    return render_template('mac.html', mac=mac)
+    #send_magic_packet(mac)
+    return render_template('index.html', computers=Computers.config())
 
 
 if __name__ == '__main__':
+    print(Computers.config())
     app.run(host='127.0.0.1', port=8080)
